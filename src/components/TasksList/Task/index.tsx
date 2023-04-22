f1f7c2e5ea88f37714d8b44f0c77e863c6efaaad
Luis Eduardo <luiseduardo20.dev@gmail.com>
2023-04-22 18:21:36 -0300

    feat: delete task

--- src/components/TasksList/Task/index.tsx
+++ src/components/TasksList/Task/index.tsx
@@ -1,13 +1,17 @@
 import React, { useState } from "react";
+import { FiTrash2 } from "react-icons/fi";
 
 import * as S from "../styles";
 import Spacer from "../../Spacer";
+import useTask from "hooks/useTask";
 
 type Props = {
   taskText: string;
 };
 
 const Task = ({ taskText }: Props) => {
+  const { handleDeleteTask } = useTask();
+
   const [checked, setChecked] = useState(false);
 
   const handleChangeCheck = () => setChecked((oldValue) => !oldValue);
@@ -15,11 +19,20 @@ const Task = ({ taskText }: Props) => {
   return (
     <>
       <S.Task>
-        <S.StyledCheckbox
-          checked={checked}
-          onClick={handleChangeCheck}
-        />
-        <S.TaskText checked={checked}>{taskText}</S.TaskText>
+        <div>
+          <S.StyledCheckbox
+            checked={checked}
+            onClick={handleChangeCheck}
+          />
+          <S.TaskText checked={checked}>{taskText}</S.TaskText>
+        </div>
+
+        <button>
+          <FiTrash2
+            onClick={() => handleDeleteTask(taskText)}
+            size={20}
+          />
+        </button>
       </S.Task>
 
       <Spacer height={2} />
--- src/hooks/useTask.tsx
+++ src/hooks/useTask.tsx
@@ -12,6 +12,7 @@ type Props = {
 type TasksContextProps = {
   tasks: string[];
   handleCreateTask: (task: string) => void;
+  handleDeleteTask: (taskName: string) => void;
 };
 
 const TasksContext = createContext({} as TasksContextProps);
@@ -19,18 +20,29 @@ const TasksContext = createContext({} as TasksContextProps);
 export const TasksProvider = ({ children }: Props) => {
   const [tasks, setTasks] = useState<string[]>([]);
 
-  const handleCreateTask = useCallback((task: string) => {
-    console.log("tasks:", tasks);
+  const handleCreateTask = useCallback(
+    (task: string) => {
+      if (!tasks.includes(task)) {
+        setTasks((oldTasks) => [task, ...oldTasks]);
+      } else {
+        console.error("Task exist on array");
+      }
+    },
+    [tasks]
+  );
 
-    if (!tasks.includes(task)) {
-      setTasks((oldTasks) => [task, ...oldTasks]);
-    } else {
-      console.error("Task exist on array");
-    }
-  }, []);
+  const handleDeleteTask = (taskName: string) => {
+    const tasksArrayWithoutClickedTask = tasks.filter(
+      (task) => task !== taskName
+    );
+
+    setTasks(tasksArrayWithoutClickedTask);
+  };
 
   return (
-    <TasksContext.Provider value={{ tasks, handleCreateTask }}>
+    <TasksContext.Provider
+      value={{ tasks, handleCreateTask, handleDeleteTask }}
+    >
       {children}
     </TasksContext.Provider>
   );
